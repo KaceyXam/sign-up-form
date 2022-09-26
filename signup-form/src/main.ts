9ccd4d363b1200c3fd9c1aec12b49db871c90da4
kaceykniffen <kaceyxam@gmail.com>
2022-09-25 20:55:08 -0500

    built the vite project

--- signup-form/src/main.ts
+++ signup-form/src/main.ts
@@ -1,11 +1,6 @@
 import "./style.css";
 
 const mainForm: HTMLFormElement = document.querySelector("#signup-form")!;
-const firstName: HTMLInputElement = mainForm.querySelector("#fname")!;
-const lastName: HTMLInputElement = mainForm.querySelector("#lname")!;
-const emailInput: HTMLInputElement = mainForm.querySelector("#email")!;
-const password: HTMLInputElement = mainForm.querySelector("#pass")!;
-const submitButton: HTMLButtonElement = mainForm.querySelector("#submit")!;
 
 mainForm.addEventListener("submit", (e) => {
   e.preventDefault();
