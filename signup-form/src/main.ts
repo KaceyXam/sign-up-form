0689d85f92e9f6a4d9b864a15b31aba1462f85a7
kaceykniffen <kaceyxam@gmail.com>
2022-09-25 20:39:35 -0500

    Added Typescript

--- signup-form/src/main.ts
+++ signup-form/src/main.ts
@@ -1,23 +1,21 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const mainForm: HTMLFormElement = document.querySelector("#signup-form")!;
+const firstName: HTMLInputElement = mainForm.querySelector("#fname")!;
+const lastName: HTMLInputElement = mainForm.querySelector("#lname")!;
+const emailInput: HTMLInputElement = mainForm.querySelector("#email")!;
+const password: HTMLInputElement = mainForm.querySelector("#pass")!;
+const submitButton: HTMLButtonElement = mainForm.querySelector("#submit")!;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+mainForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formInputs = mainForm.querySelectorAll("input")!;
+
+  formInputs.forEach((node) => {
+    if (node.value === "") {
+      node.parentElement?.classList.add("error");
+    } else {
+      node.parentElement?.classList.remove("error");
+    }
+  });
+});
